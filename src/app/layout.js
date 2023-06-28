13981d69f85a9843835df2f3b4bc218ea0f5b820
feraylane <feraylane1@gmail.com>
2023-06-29 02:11:59 +0900

    siap landing4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import Footer from './components/footer'
+import Header from './components/header'
+import { AuthProvider } from './context/authContext'
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +11,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      <body>{children}</body>
+      <body suppressHydrationWarning={true} className='bg-quinary'>
+        <AuthProvider>
+          <Header/>
+          <div>
+
+        {children}
+          </div>
+        <Footer/>
+        </AuthProvider>
+        </body>
     </html>
   )
 }
